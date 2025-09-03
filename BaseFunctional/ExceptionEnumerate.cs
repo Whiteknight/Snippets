d70bde2b413d54a51781b48ba8c7db1a7f71d154
Whiteknight <wknight8111@gmail.com>
2025-09-02 20:36:40 -0400

    A few methods to work with Exceptions
    Exception.Enumerate() goes through a full exception hierarchy and enumerates all inner exceptions
    Exception.GetInnermostException() tries to find the inner-most exception from a tree and returns it

---
+++ BaseFunctional/ExceptionEnumerate.cs
@@ -0,0 +1,36 @@
+﻿using static BaseFunctional.Assert;
+
+namespace BaseFunctional;
+
+public static class ExceptionEnumerateExtensions
+{
+    public static IEnumerable<Exception> Enumerate(this Exception exception)
+    {
+        var worklist = new Queue<Exception>();
+        worklist.Enqueue(NotNull(exception));
+
+        static void AddAggregateInners(AggregateException agg, Queue<Exception> list)
+        {
+            foreach (var inner in agg.InnerExceptions)
+                list.Enqueue(inner);
+        }
+
+        while (worklist.Count != 0)
+        {
+            var ex = worklist.Dequeue();
+            if (ex is AggregateException agg)
+                AddAggregateInners(agg, worklist);
+            else if (ex.InnerException is not null)
+                worklist.Enqueue(ex.InnerException);
+            yield return ex;
+        }
+    }
+
+    public static Exception GetInnermostException(this Exception exception)
+    {
+        var ex = NotNull(exception);
+        while (ex.InnerException is not null)
+            ex = ex.InnerException;
+        return ex;
+    }
+}
