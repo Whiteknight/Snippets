ca58b8ce811de44a2e33a6ee647a270708559a7e
Whiteknight <wknight8111@gmail.com>
2025-09-02 20:34:59 -0400

    Optimize IEnumerable<T>.Batch() for different collection types

--- BaseFunctional/Enumerable.Batch.cs
+++ BaseFunctional/Enumerable.Batch.cs
@@ -4,23 +4,46 @@ namespace BaseFunctional;
 
 public static class BatchExtensions
 {
-    public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, int batchSize)
+    public static IEnumerable<IReadOnlyList<T>> Batch<T>(this IEnumerable<T> source, int batchSize)
     {
-        NotZeroOrNegative(batchSize);
-        var batch = new List<T>(batchSize);
-        int count = 0;
-        foreach (var item in source.OrEmptyIfNull())
+        static IEnumerable<IReadOnlyList<T>> IterateNonList(IEnumerable<T> source, int batchSize)
         {
-            batch.Add(item);
-            count++;
-            if (count == batchSize)
+            var batch = new List<T>(batchSize);
+            int count = 0;
+            foreach (var item in source.OrEmptyIfNull())
             {
-                yield return batch;
-                batch = new List<T>(batchSize);
+                batch.Add(item);
+                count++;
+                if (count == batchSize)
+                {
+                    yield return batch;
+                    batch = new List<T>(batchSize);
+                    count = 0;
+                }
             }
+
+            if (batch.Count > 0)
+                yield return batch;
         }
 
-        if (batch.Count > 0)
-            yield return batch;
+        static IEnumerable<IReadOnlyList<T>> IterateList(List<T> source, int batchSize)
+        {
+            for (int i = 0; i < source.Count; i += batchSize)
+                yield return source.Slice(i, Math.Min(source.Count - i, batchSize));
+        }
+
+        static IEnumerable<IReadOnlyList<T>> IterateArray(T[] source, int batchSize)
+        {
+            for (int i = 0; i < source.Length; i += batchSize)
+                yield return new ArraySegment<T>(source, i, Math.Min(source.Length - i, batchSize)).ToArray();
+        }
+
+        NotZeroOrNegative(batchSize);
+        return NotNull(source) switch
+        {
+            List<T> list => IterateList(list, batchSize),
+            T[] array => IterateArray(array, batchSize),
+            _ => IterateNonList(source, batchSize)
+        };
     }
 }
