cd3423862564cd483f57528b825d76be1e4b4e61
Whiteknight <wknight8111@gmail.com>
2025-09-02 20:35:45 -0400

    A few helper methods to for int ranges and date day ranges

---
+++ BaseFunctional/DateTimeRanges.cs
@@ -0,0 +1,13 @@
+﻿namespace BaseFunctional;
+
+public static class DateTimeRangesExtensions
+{
+    public static bool IsWithinDaysOf(this DateTime thisDate, DateTime now, int daysMin, int daysMax)
+    {
+        var daysDiff = (int)Math.Round((now - thisDate).TotalDays, MidpointRounding.AwayFromZero);
+        return daysDiff.IsBetweenInclusive(daysMin, daysMax);
+    }
+
+    public static bool IsWithinDaysOfNow(this DateTime thisDate, int daysMin, int daysMax)
+        => thisDate.IsWithinDaysOf(DateTime.UtcNow, daysMin, daysMax);
+}
---
+++ BaseFunctional/Int.IsBetween.cs
@@ -0,0 +1,9 @@
+﻿namespace BaseFunctional;
+
+public static class IntBetweenExtensions
+{
+    public static bool IsBetweenInclusive(this int thisInt, int min, int max)
+        => min <= max
+            ? min <= thisInt && thisInt <= max
+            : throw new ArgumentException("Minimum is larger than maximum. Comparison is invalid.");
+}
