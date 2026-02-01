fe4843d062bab44812ff2fa4ab34f13bf84e5116
Whiteknight <wknight8111@gmail.com>
2026-02-01 14:24:19 -0500

    Add a few new data->security errors

---
+++ BaseFunctional/Errors/ConfigurationKeyMissing.cs
@@ -0,0 +1,87 @@
+﻿namespace BaseFunctional.Errors;
+
+public sealed record ConfigurationKeyMissing(string Name, string Description)
+    : Error($"Missing configuration value '{Name}'. Expected {Description}")
+{
+    public static ConfigurationKeyMissing Url(string name)
+        => new ConfigurationKeyMissing(name, "URL");
+
+    public static ConfigurationKeyMissing DbConnectionString(string name)
+        => new ConfigurationKeyMissing(name, "database connection string");
+
+    public static ConfigurationKeyMissing BooleanFlag(string name)
+        => new ConfigurationKeyMissing(name, "boolean flag");
+
+    public static ConfigurationKeyMissing Integer(string name)
+        => new ConfigurationKeyMissing(name, "integer");
+}
+
+// When configuration is missing, it is often a show-stopper for application startup. In these
+// cases we may want to raise an exception and bring the whole application down.
+public sealed class ConfigurationKeyMissingException : Exception
+{
+    public ConfigurationKeyMissingException(string name, string description)
+        : base($"Missing configuration value '{name}'. Expected {description}")
+    {
+    }
+
+    public ConfigurationKeyMissingException(string name, string description, Exception inner)
+        : base($"Missing configuration value '{name}'. Expected {description}", inner)
+    {
+    }
+
+    public static ConfigurationKeyMissingException Url(string name)
+        => new ConfigurationKeyMissingException(name, "URL");
+
+    public static ConfigurationKeyMissingException DbConnectionString(string name)
+        => new ConfigurationKeyMissingException(name, "database connection string");
+
+    public static ConfigurationKeyMissingException BooleanFlag(string name)
+        => new ConfigurationKeyMissingException(name, "boolean flag");
+
+    public static ConfigurationKeyMissing Integer(string name)
+        => new ConfigurationKeyMissing(name, "integer");
+}
+
+// Similar to a ConfigurationKeyMissing error, but strictly for Environment Variables.
+public sealed record EnvironmentVariableMissing(string Name, string Description)
+    : Error($"Missing environment variable '{Name}'. Expected {Description}")
+{
+    public static EnvironmentVariableMissing Url(string name)
+        => new EnvironmentVariableMissing(name, "URL");
+
+    public static EnvironmentVariableMissing DbConnectionString(string name)
+        => new EnvironmentVariableMissing(name, "database connection string");
+
+    public static EnvironmentVariableMissing BooleanFlag(string name)
+        => new EnvironmentVariableMissing(name, "boolean flag");
+
+    public static EnvironmentVariableMissing Integer(string name)
+        => new EnvironmentVariableMissing(name, "integer");
+}
+
+// Similar to ConfigurationKeyMissingException, but strictly for environment variables.
+public sealed class EnvironmentVariableMissingException : Exception
+{
+    public EnvironmentVariableMissingException(string name, string description)
+        : base($"Missing configuration value '{name}'. Expected {description}")
+    {
+    }
+
+    public EnvironmentVariableMissingException(string name, string description, Exception inner)
+        : base($"Missing configuration value '{name}'. Expected {description}", inner)
+    {
+    }
+
+    public static EnvironmentVariableMissingException Url(string name)
+        => new EnvironmentVariableMissingException(name, "URL");
+
+    public static EnvironmentVariableMissingException DbConnectionString(string name)
+        => new EnvironmentVariableMissingException(name, "database connection string");
+
+    public static EnvironmentVariableMissingException BooleanFlag(string name)
+        => new EnvironmentVariableMissingException(name, "boolean flag");
+
+    public static EnvironmentVariableMissingException Integer(string name)
+        => new EnvironmentVariableMissingException(name, "integer");
+}
---
+++ BaseFunctional/Errors/InvalidParameter.cs
@@ -0,0 +1,16 @@
+﻿namespace BaseFunctional.Errors;
+
+// Values provided by a user are either incomplete or invalid.
+// This likely turns into an HTTP 400 Bad Request
+public sealed record InvalidParameter(string[] Missing, string[] Invalid)
+    : Error(FormatMessage(Missing, Invalid))
+{
+    private static string FormatMessage(string[] missing, string[] invalid)
+    {
+        if ((missing == null || missing.Length == 0) && (invalid == null || invalid.Length == 0))
+            return "Parameters are invalid";
+        var items = missing.OrEmptyIfNull().Select(m => $"Missing: {m}")
+            .Concat(invalid.OrEmptyIfNull().Select(i => $"Invalid: {i}"));
+        return $"Problems with provided parameters:\n{string.Join("\n", items)}";
+    }
+}
