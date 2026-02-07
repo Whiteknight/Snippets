73eed9a18498c726591d729add3b85f4103676c5
Whiteknight <wknight8111@gmail.com>
2026-02-07 08:18:05 -0500

    DisposablesList that we can use to track multiple disposables and clean up at once
    Useful for places such as test scenarios where we may need to create multiple objects during the test, but want to clean everything up in a single method without knowledge or what we created or not

---
+++ BaseFunctional/Disposables.cs
@@ -0,0 +1,12 @@
+﻿namespace BaseFunctional;
+
+public readonly record struct DisposablesList(List<IDisposable> Disposables) : IDisposable
+{
+    public void Dispose()
+    {
+        foreach (var disposable in Disposables)
+            disposable.Dispose();
+
+        Disposables.Clear();
+    }
+}
